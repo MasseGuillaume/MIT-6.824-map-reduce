dc62dbabe86d46076e55c68113f63549dca13550
Guillaume Massé <masgui@gmail.com>
2022-01-28 22:35:20 -0500

    uncomment all tests

--- map-reduce/test-mr.sh
+++ map-reduce/test-mr.sh
@@ -7,204 +7,205 @@ cd mr-tmp || exit 1
 
 failed_any=0
  
+# clear terminal
 printf "\u001bc"
 
-# #########################################################
-# echo '***' Starting word count test.
+#########################################################
+echo '***' Starting word count test.
 
-# # Word Count
-# ../mrsequential ../apps/wordcount.jar WordCount ../data/pg*txt || exit 1
-# sort mr-out-0 > mr-correct-wc.txt
-# rm -f mr-out*
+# Word Count
+../mrsequential ../apps/wordcount.jar WordCount ../data/pg*txt || exit 1
+sort mr-out-0 > mr-correct-wc.txt
+rm -f mr-out*
 
-# # start multiple workers.
-# timeout -k 2s 180s ../mrworker ../apps/wordcount.jar WordCount 0 3 &
-# timeout -k 2s 180s ../mrworker ../apps/wordcount.jar WordCount 1 3 &
-# timeout -k 2s 180s ../mrworker ../apps/wordcount.jar WordCount 2 3 &
+# start multiple workers.
+timeout -k 2s 180s ../mrworker ../apps/wordcount.jar WordCount 0 3 &
+timeout -k 2s 180s ../mrworker ../apps/wordcount.jar WordCount 1 3 &
+timeout -k 2s 180s ../mrworker ../apps/wordcount.jar WordCount 2 3 &
 
-# echo '-- wait 2s for workers to start --'
-# sleep 2
+echo '-- wait 2s for workers to start --'
+sleep 2
 
-# timeout -k 2s 180s ../mrcoordinator 3 ../data/pg*txt &
-# pid=$!
+timeout -k 2s 180s ../mrcoordinator 3 ../data/pg*txt &
+pid=$!
 
-# # wait for the coordinator to exit.
-# wait $pid
+# wait for the coordinator to exit.
+wait $pid
 
-# # since workers are required to exit when a job is completely finished,
-# # and not before, that means the job has finished.
-# sort mr-out* | grep . > mr-wc-all
-# if cmp mr-wc-all mr-correct-wc.txt
-# then
-#   echo '---' wc test: PASS
-# else
-#   echo '---' wc output is not the same as mr-correct-wc.txt
-#   echo '---' wc test: FAIL
-#   failed_any=1
-# fi
+# since workers are required to exit when a job is completely finished,
+# and not before, that means the job has finished.
+sort mr-out* | grep . > mr-wc-all
+if cmp mr-wc-all mr-correct-wc.txt
+then
+  echo '---' wc test: PASS
+else
+  echo '---' wc output is not the same as mr-correct-wc.txt
+  echo '---' wc test: FAIL
+  failed_any=1
+fi
 
-# wait # for workers and coordinator to exit 
+wait # for workers and coordinator to exit 
 
-# #########################################################
-# echo '***' Starting indexer.
+#########################################################
+echo '***' Starting indexer.
 
-# # Indexer
-# ../mrsequential ../apps/indexer.jar Indexer ../data/pg*txt || exit 1
-# sort mr-out-0 > mr-correct-indexer.txt
-# rm -f mr-out*
+# Indexer
+../mrsequential ../apps/indexer.jar Indexer ../data/pg*txt || exit 1
+sort mr-out-0 > mr-correct-indexer.txt
+rm -f mr-out*
 
-# # start multiple workers.
-# timeout -k 2s 180s ../mrworker ../apps/indexer.jar Indexer 0 2 &
-# timeout -k 2s 180s ../mrworker ../apps/indexer.jar Indexer 1 2 &
+# start multiple workers.
+timeout -k 2s 180s ../mrworker ../apps/indexer.jar Indexer 0 2 &
+timeout -k 2s 180s ../mrworker ../apps/indexer.jar Indexer 1 2 &
 
-# echo '-- wait 2s for workers to start --'
-# sleep 2
+echo '-- wait 2s for workers to start --'
+sleep 2
 
-# timeout -k 2s 180s ../mrcoordinator 2 ../data/pg*txt &
-# pid=$!
+timeout -k 2s 180s ../mrcoordinator 2 ../data/pg*txt &
+pid=$!
 
-# # wait for the coordinator to exit.
-# wait $pid
+# wait for the coordinator to exit.
+wait $pid
 
-# sort mr-out* | grep . > mr-indexer-all
-# if cmp mr-indexer-all mr-correct-indexer.txt
-# then
-#   echo '---' indexer test: PASS
-# else
-#   echo '---' indexer output is not the same as mr-correct-indexer.txt
-#   echo '---' indexer test: FAIL
-#   failed_any=1
-# fi
+sort mr-out* | grep . > mr-indexer-all
+if cmp mr-indexer-all mr-correct-indexer.txt
+then
+  echo '---' indexer test: PASS
+else
+  echo '---' indexer output is not the same as mr-correct-indexer.txt
+  echo '---' indexer test: FAIL
+  failed_any=1
+fi
 
-# wait # for workers and coordinator to exit 
+wait # for workers and coordinator to exit 
 
-# #########################################################
-# echo '***' Starting map parallelism test.
+#########################################################
+echo '***' Starting map parallelism test.
 
-# rm -f mr-*
+rm -f mr-*
 
-# timeout -k 2s 180s ../mrworker ../apps/map-parallel.jar MapParallel 0 2 &
-# timeout -k 2s 180s ../mrworker ../apps/map-parallel.jar MapParallel 1 2 &
+timeout -k 2s 180s ../mrworker ../apps/map-parallel.jar MapParallel 0 2 &
+timeout -k 2s 180s ../mrworker ../apps/map-parallel.jar MapParallel 1 2 &
 
-# echo '-- wait 2s for workers to start --'
-# sleep 2
+echo '-- wait 2s for workers to start --'
+sleep 2
 
-# timeout -k 2s 180s ../mrcoordinator 2 ../data/pg*txt # << block
+timeout -k 2s 180s ../mrcoordinator 2 ../data/pg*txt # << block
 
-# NT=`cat mr-out* | grep '^times-' | wc -l | sed 's/ //g'`
-# if [ "$NT" != "2" ]
-# then
-#   echo '---' saw "$NT" workers rather than 2
-#   echo '---' map parallelism test: FAIL
-#   failed_any=1
-# fi
+NT=`cat mr-out* | grep '^times-' | wc -l | sed 's/ //g'`
+if [ "$NT" != "2" ]
+then
+  echo '---' saw "$NT" workers rather than 2
+  echo '---' map parallelism test: FAIL
+  failed_any=1
+fi
 
-# if cat mr-out* | grep '^parallel.* 2' > /dev/null
-# then
-#   echo '---' map parallelism test: PASS
-# else
-#   echo '---' map workers did not run in parallel
-#   echo '---' map parallelism test: FAIL
-#   failed_any=1
-# fi
+if cat mr-out* | grep '^parallel.* 2' > /dev/null
+then
+  echo '---' map parallelism test: PASS
+else
+  echo '---' map workers did not run in parallel
+  echo '---' map parallelism test: FAIL
+  failed_any=1
+fi
 
-# wait # for workers to exit
+wait # for workers to exit
 
-# #########################################################
-# echo '***' Starting reduce parallelism test.
+#########################################################
+echo '***' Starting reduce parallelism test.
 
-# rm -f mr-*
+rm -f mr-*
 
-# timeout -k 2s 180s ../mrworker ../apps/reduce-parallel.jar ReduceParallel 0 2 &
-# timeout -k 2s 180s ../mrworker ../apps/reduce-parallel.jar ReduceParallel 1 2 &
+timeout -k 2s 180s ../mrworker ../apps/reduce-parallel.jar ReduceParallel 0 2 &
+timeout -k 2s 180s ../mrworker ../apps/reduce-parallel.jar ReduceParallel 1 2 &
 
-# echo '-- wait 2s for workers to start --'
-# sleep 2
+echo '-- wait 2s for workers to start --'
+sleep 2
 
-# timeout -k 2s 180s ../mrcoordinator 2 ../data/pg*txt # << block
+timeout -k 2s 180s ../mrcoordinator 2 ../data/pg*txt # << block
 
-# NT=`cat mr-out* | grep '^[a-z] 2' | wc -l | sed 's/ //g'`
-# if [ "$NT" -lt "2" ]
-# then
-#   echo '---' too few parallel reduces.
-#   echo '---' reduce parallelism test: FAIL
-#   failed_any=1
-# else
-#   echo '---' reduce parallelism test: PASS
-# fi
+NT=`cat mr-out* | grep '^[a-z] 2' | wc -l | sed 's/ //g'`
+if [ "$NT" -lt "2" ]
+then
+  echo '---' too few parallel reduces.
+  echo '---' reduce parallelism test: FAIL
+  failed_any=1
+else
+  echo '---' reduce parallelism test: PASS
+fi
 
-# wait
+wait
 
-# # Gui: This test does not really make sense
-# #########################################################
-# echo '***' Starting job count test.
+# Gui: This test does not really make sense
+#########################################################
+echo '***' Starting job count test.
 
-# rm -f mr-*
+rm -f mr-*
 
-# timeout -k 2s 180s ../mrworker ../apps/jobcount.jar JobCount 0 2 &
-# timeout -k 2s 180s ../mrworker ../apps/jobcount.jar JobCount 1 2 &
-
-
-# echo '-- wait 2s for workers to start --'
-# sleep 2
-
-# timeout -k 2s 180s ../mrcoordinator 2 ../data/pg*txt # << block
-
-# NT=`cat mr-out* | awk '{print $2}'`
-# if [ "$NT" -ne "8" ]
-# then
-#   echo '---' map jobs ran incorrect number of times "($NT != 8)"
-#   echo '---' job count test: FAIL
-#   failed_any=1
-# else
-#   echo '---' job count test: PASS
-# fi
-
-# wait
-
-# #########################################################
-# test whether any worker or coordinator exits before the
-# task has completed (i.e., all output files have been finalized)
-# rm -f mr-*
-
-# echo '***' Starting early exit test.
+timeout -k 2s 180s ../mrworker ../apps/jobcount.jar JobCount 0 2 &
+timeout -k 2s 180s ../mrworker ../apps/jobcount.jar JobCount 1 2 &
 
 
-# # start multiple workers.
-# timeout -k 2s 180s ../mrworker ../apps/early-exit.jar EarlyExit0 0 3 &
-# timeout -k 2s 180s ../mrworker ../apps/early-exit.jar EarlyExit0 1 3 &
-# timeout -k 2s 180s ../mrworker ../apps/early-exit.jar EarlyExit0 2 3 &
+echo '-- wait 2s for workers to start --'
+sleep 2
 
-# echo '-- wait 2s for workers to start --'
-# sleep 2
+timeout -k 2s 180s ../mrcoordinator 2 ../data/pg*txt # << block
 
-# timeout -k 2s 180s ../mrcoordinator 3 ../data/pg*txt &
-# pid=$!
+NT=`cat mr-out* | awk '{print $2}'`
+if [ "$NT" -ne "8" ]
+then
+  echo '---' map jobs ran incorrect number of times "($NT != 8)"
+  echo '---' job count test: FAIL
+  failed_any=1
+else
+  echo '---' job count test: PASS
+fi
 
-# # wait for any of the coord or workers to exit
-# # `jobs` ensures that any completed old processes from other tests
-# # are not waited upon
-# jobs &> /dev/null
-# wait -n
+wait
 
-# # a process has exited. this means that the output should be finalized
-# # otherwise, either a worker or the coordinator exited early
-# sort mr-out* | grep . > mr-wc-all-initial
+#########################################################
+test whether any worker or coordinator exits before the
+task has completed (i.e., all output files have been finalized)
+rm -f mr-*
 
-# # wait for remaining workers and coordinator to exit.
-# wait
+echo '***' Starting early exit test.
 
-# # compare initial and final outputs
-# sort mr-out* | grep . > mr-wc-all-final
-# if cmp mr-wc-all-final mr-wc-all-initial
-# then
-#   echo '---' early exit test: PASS
-# else
-#   echo '---' output changed after first worker exited
-#   echo '---' early exit test: FAIL
-#   failed_any=1
-# fi
-# rm -f mr-*
+
+# start multiple workers.
+timeout -k 2s 180s ../mrworker ../apps/early-exit.jar EarlyExit0 0 3 &
+timeout -k 2s 180s ../mrworker ../apps/early-exit.jar EarlyExit0 1 3 &
+timeout -k 2s 180s ../mrworker ../apps/early-exit.jar EarlyExit0 2 3 &
+
+echo '-- wait 2s for workers to start --'
+sleep 2
+
+timeout -k 2s 180s ../mrcoordinator 3 ../data/pg*txt &
+pid=$!
+
+# wait for any of the coord or workers to exit
+# `jobs` ensures that any completed old processes from other tests
+# are not waited upon
+jobs &> /dev/null
+wait -n
+
+# a process has exited. this means that the output should be finalized
+# otherwise, either a worker or the coordinator exited early
+sort mr-out* | grep . > mr-wc-all-initial
+
+# wait for remaining workers and coordinator to exit.
+wait
+
+# compare initial and final outputs
+sort mr-out* | grep . > mr-wc-all-final
+if cmp mr-wc-all-final mr-wc-all-initial
+then
+  echo '---' early exit test: PASS
+else
+  echo '---' output changed after first worker exited
+  echo '---' early exit test: FAIL
+  failed_any=1
+fi
+rm -f mr-*
 
 #########################################################
 echo '***' Starting crash test.
@@ -257,6 +258,6 @@ else
 fi
 
 
-# # # kill possible remaining processes
+# kill possible remaining processes
 # kill $(jps | grep worker | awk '{print $1}')
 # kill $(jps | grep coordinator | awk '{print $1}')
